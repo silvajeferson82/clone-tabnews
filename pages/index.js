c2a7a5736ccedebbba45ffe54e7b71f5da0d9db7
Jeferson SIlva <34014937+silvajeferson82@users.noreply.github.com>
2025-06-08 15:08:30 +0000

    add scripts `lint:chech` and `lint:fix` to package.json

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-  return <h1>Minha princesa, te amo...Beijos &#128525; &#128151;</h1>  
+function Home() {
+  return <h1>Minha princesa, te amo...Beijos &#128525; &#128151;</h1>;
 }
 
 export default Home;
