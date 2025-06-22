48c61c549b4164a607654d2f735bb39b5a7f84ab
Jeferson Silva <jefersonsilva.contato@gmail.com>
2025-06-22 18:46:32 -0300

    make databse.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,14 @@ async function query(queryObject) {
     port: parseInt(process.env.POSTGRES_PORT, 10),
   });
   await client.connect();
-  const result  = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
