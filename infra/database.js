007588f9e9570f009bc765a5d5170a707b7887e8
Jeferson Silva <jefersonsilva.contato@gmail.com>
2025-07-03 11:50:05 -0300

    console to show credentials error

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,13 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     port: parseInt(process.env.POSTGRES_PORT, 10),
   });
+  console.log("credenciais PG:", {
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
+  })
   try {
     await client.connect();
     const result = await client.query(queryObject);
